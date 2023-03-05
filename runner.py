5662344f7d710ca6be31a0aff3978176b6420d2c
Igor Petrov <ds9657@yandex.ru>
2023-03-05 19:46:40 +0600

    Update runner.py
    Добавлена проверка наличия папок и их создание при их отсутствии.

--- runner.py
+++ runner.py
@@ -6,9 +6,15 @@ import docx_save_print
 
 
 def main():
+    # Проверка наличия папок
+    results, images = 'Results', 'Images'
+    if not os.path.exists(results):
+        os.mkdir(results)
+        os.mkdir(f"{results}/{images}")
+        print("Все необходимые папки созданы")
+
     find_get_image.Find_Get()
     translater.Translate()
-
     with open('Results/translate.txt', 'r', encoding='utf-8') as translate:
         url_list = []
         for stroke in translate.read().split('\n'):
